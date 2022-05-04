09354b080946b0478e56e334ce8ed51628ec9f9e
CarlosBricen0 <carlos.b.cantillana@gmail.com>
2022-05-04 10:21:57 -0400

    add to repository

--- selenium/index.js
+++ selenium/index.js
@@ -1,11 +1,28 @@
-const {Builder, By, Key, until} = require('selenium-webdriver');
+import { Builder, By, until } from 'selenium-webdriver';
 
 (async function example() {
-  let driver = await new Builder().forBrowser('chrome').build();
+  let driver = new Builder().forBrowser('chrome').build();
   try {
-    await driver.get('http://www.google.com/ncr');
-    await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
-   // await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
+    await driver.get('https://www.instagram.com/');
+    await driver.wait(until.elementLocated(By.name('username')), 10 * 1000);
+    await driver.findElement(By.name('username')).sendKeys('aiep.briceno@gmail.com');
+    await driver.wait(until.elementLocated(By.name('password')), 10 * 1000);
+    await driver.findElement(By.name('password')).sendKeys('carlosaiep');
+    await driver.findElement(By.css('#loginForm > div > div:nth-child(3)')).click();
+    await driver.wait(until.elementLocated(By.className('wpO6b')), 10 * 1000);
+    await driver.findElement(By.className('wpO6b')).click();
+    await driver.wait(until.elementLocated(By.className('sqdOP')), 10 * 1000);
+    await driver.findElement(By.className('sqdOP')).click();
+    await driver.wait(until.elementLocated(By.className('yWX7d')), 10 * 1000);
+    await driver.findElement(By.className('yWX7d')).click();
+    await driver.wait(until.elementLocated(By.className('sqdOP')), 10 * 1000);
+    await driver.findElement(By.className('sqdOP')).click();
+    await driver.wait(until.elementLocated(By.className('PUqUI')), 10 * 1000);
+    await driver.findElement(By.name('PUqUI')).sendKeys('automatizando AIEP');
+    await driver.wait(until.elementLocated(By.className('sqdOP')), 10 * 1000);
+    await driver.findElement(By.className('sqdOP')).click();
+    await driver.wait(until.elementLocated(By.name('s4Iyt')), 10 * 1000);
+    await driver.wait(until.elementLocated(By.name('_2hvTZ')), 10 * 1000);
   } finally {
     await driver.quit();
   }
